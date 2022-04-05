278a786f044a0a827e0282e25d96b212b2767e26
mrxiaozhuox <mrxzx@wwsg18.top>
2022-04-05 16:25:56 +0800

    feat: commit code

--- src/elements/button.rs
+++ src/elements/button.rs
@@ -1,8 +1,45 @@
 use dioxus::prelude::*;
 
+use crate::{Colors, Sizes};
+
+pub enum ButtonState {
+    Normal,
+    Hover,
+    Focus,
+    Active,
+    Loading,
+    Static,
+    Disabled,
+}
+
+impl Default for ButtonState {
+    fn default() -> Self {
+        Self::Normal
+    }
+}
+
+#[derive(Props)]
 pub struct ButtonProps<'a> {
+    #[props(optional)]
+    color: Option<Colors>,
 
+    #[props(optional)]
+    size: Option<Sizes>,
 
+    #[props(default)]
+    is_light: bool,
+
+    #[props(default)]
+    is_outlined: bool,
+
+    #[props(default)]
+    is_inverted: bool,
+
+    #[props(default)]
+    is_rounded: bool,
+
+    #[props(default)]
+    is_fullwidth: bool,
 
     children: Element<'a>,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(non_snake_case)]
 
 pub mod columns;
-pub mod elements;
 pub mod components;
+pub mod elements;
 
 /// include bulma with css content
 pub fn get_bulma_css() -> &'static str {
@@ -14,10 +14,59 @@ pub fn get_bulma_cdn() -> &'static str {
     "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
 }
 
+#[derive(Clone)]
 pub enum Colors {
     Primary,
     Link,
     Info,
     Success,
-    
+    Warning,
+    Danger,
+    Black,
+    Dark,
+    Light,
+    White,
+}
+
+impl ToString for Colors {
+    fn to_string(&self) -> String {
+        match self {
+            Colors::Primary => "primary",
+            Colors::Link => "link",
+            Colors::Info => "info",
+            Colors::Success => "success",
+            Colors::Warning => "warning",
+            Colors::Danger => "danger",
+            Colors::Black => "black",
+            Colors::Dark => "dark",
+            Colors::Light => "light",
+            Colors::White => "white",
+        }
+        .to_string()
+    }
+}
+
+pub enum Sizes {
+    Small,
+    Normal,
+    Medium,
+    Large,
+}
+
+impl ToString for Sizes {
+    fn to_string(&self) -> String {
+        match self {
+            Sizes::Small => "small",
+            Sizes::Normal => "normal",
+            Sizes::Medium => "medium",
+            Sizes::Large => "large",
+        }
+        .to_string()
+    }
+}
+
+impl Default for Sizes {
+    fn default() -> Self {
+        Self::Normal
+    }
 }
